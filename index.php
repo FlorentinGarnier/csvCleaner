4e85331b4648ae6229a007b4c1c249e70cf177b9
florentingarnier <garnier.florentin@gmail.com>
2017-02-03 18:20:17 +0100

    selection of which character mark EOL + bootstrapisation

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 /**
  * Model
  */
-$csv = null;
 $output = null;
-if (isset($_FILES['csv'])) {
+if (isset($_FILES['csv']) && isset($_POST['character'])) {
+    $EOLCharacter = $_POST['character'];
     //Errors
     if ($_FILES['csv']['error'] > 0) {
         switch ($_FILES['csv']['error']) {
@@ -33,10 +33,9 @@ if (isset($_FILES['csv'])) {
     }
 
 
-
     $csvHandle = fopen($_FILES['csv']['tmp_name'], 'r+');
     while ($buffer = fgets($csvHandle)) {
-        if (substr($buffer, -2) != '"'.chr(10)) {
+        if (substr($buffer, -2) != $EOLCharacter . chr(10)) {
             $buffer = str_replace([chr(13), chr(10)], '', $buffer);
             $output .= $buffer;
 
@@ -45,13 +44,12 @@ if (isset($_FILES['csv'])) {
         }
 
 
-
     }
     fseek($csvHandle, 0);
     fwrite($csvHandle, $output);
     fclose($csvHandle);
 
-    rename($_FILES['csv']['tmp_name'], 'output/'. $_FILES['csv']['name']);
+    rename($_FILES['csv']['tmp_name'], 'output/' . $_FILES['csv']['name']);
 
 
 }
@@ -66,30 +64,50 @@ if (isset($_FILES['csv'])) {
 
     <title>CSV File Newline Cleaner</title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
 <body>
-<h1>Welcome to CSV File Newline Cleaner</h1>
-<h2>This is a tools to remove newline and carriage return in csv file</h2>
-<form action="" method="post" enctype="multipart/form-data">
-    <label for="csv">CSV File</label>
-    <input type="hidden" name="MAX_FILE_SIZE" value="10485760"/>
-    <input type="file" name="csv"><br>
-    <input type="submit" value="Send">
-</form>
-<div>
-    <?php
-    if ($output) {
-        echo '<a href="output/'.$_FILES['csv']['name'].'">Download corrected file</a>';
-    }
-    ?>
+<div class="container">
+    <h1>Welcome to CSV File Newline Cleaner</h1>
+    <h2>This is a tools to remove newline and carriage return in csv file</h2>
+    <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label class="col-sm-2 control-label" for="csv">CSV File</label>
+            <div class="col-sm-10">
+                <input type="hidden" name="MAX_FILE_SIZE" value="10485760"/>
+                <input type="file" name="csv">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-2 control-label" for="character">Which character is EOL</label>
+            <div class="col-sm-10">
+                <input type="text" name="character" required>
+            </div>
+        </div>
+        <input class="btn btn-success" type="submit" value="Send">
+    </form>
+    <div>
+        <?php
+        if ($output) {
+            echo '<a href="output/' . $_FILES['csv']['name'] . '">Download corrected file</a>';
+        }
+        ?>
+    </div>
+
+    <footer>
+        <p>
+            Florentin Garnier
+            - <a href="mailto:garnier.florentin@gmail.com">garnier.florentin@gmail.com</a>
+            - <a href="https://twitter.com/garnierfl">Twitter</a>
+            - <a href="https://github.com/florentingarnier">Github</a>
+        </p>
+    </footer>
 </div>
-<footer>
-    <p>
-        Florentin Garnier
-        - <a href="mailto:garnier.florentin@gmail.com">garnier.florentin@gmail.com</a>
-        - <a href="https://twitter.com/garnierfl">Twitter</a>
-        - <a href="https://github.com/florentingarnier">Github</a>
-    </p>
-</footer>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
